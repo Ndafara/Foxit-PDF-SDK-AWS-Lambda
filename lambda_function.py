4502dd44641be4b171a1ca8c64e132cb6e0daebc
Ndafara <ntsamba@gmail.com>
2023-09-21 12:04:38 +0200

    Update lambda_function

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,6 @@
 from FoxitPDFSDKPython3 import fsdk
 import json
+import boto3
 
 def lambda_handler(event, context):
 
@@ -13,8 +14,19 @@ def lambda_handler(event, context):
 
     # Create a new page
     page = document.InsertPage(-1, 595, 842)
+    
+    # Create the PDF in Lambda temporary folder
+    pdf_filename = "/tmp/lambda_doc.pdf"
+    document.SaveAs(pdf_filename, fsdk.PDFDoc.e_SaveFlagNoOriginal)
+    
+    # Upload the PDF to S3
+    s3_bucket_name = "foxit-lambda-demo"
+    s3_key = "lambda_doc.pdf"
 
-    response = "Successfully created PDF from Lambda to s3 using Foxit!'"
+    s3 = boto3.client("s3")
+    s3.upload_file(pdf_filename, s3_bucket_name, s3_key)
+
+    response = "Successfully created PDF from Lambda to S3 using the Foxit PDF SDK!'"
 
     return {
         'statusCode': 200,
